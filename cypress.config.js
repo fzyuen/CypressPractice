94bc6cb785536af0dbdcf80f959e59775fb6b992
fzyuen <fzyuen@gmail.com>
2026-09-07 16:24:12 +0800

    configure test report - JUNIT

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "junit",
+  reporterOptions: {
+    mochaFile: "results/junit-[hash].xml",
+    toConsole: true,
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      
     },
   },
 });
