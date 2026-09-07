ae3c26ce801c1ddc7582f67c782747dc6df172ab
fzyuen <fzyuen@gmail.com>
2026-09-07 16:35:29 +0800

    correct the junit

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: "junit",
+  reporter: "mocha-junit-reporter",
   reporterOptions: {
     mochaFile: "results/junit-[hash].xml",
     toConsole: true,
