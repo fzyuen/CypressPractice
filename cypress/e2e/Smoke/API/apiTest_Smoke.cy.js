6ac102bd9344bd0be26af72fc72064b63d9f917d
fzyuen <fzyuen@gmail.com>
2026-09-08 12:11:41 +0800

    update import statements and test case names in API test files to include "SO-" prefix for Smoke tests and "R-" prefix for Regression tests. Also, fix the URL in the getUsers method of the APIService class to correctly append '/1' to the base URL.

--- cypress/e2e/Smoke/API/apiTest_Smoke.cy.js
+++ cypress/e2e/Smoke/API/apiTest_Smoke.cy.js
@@ -1,4 +1,4 @@
-import {APIService} from '../../../../service/apiServiceClass'
+import { APIService } from '../../../service/apiServiceClass';
 
 describe('API Test Suite', () => {
   const api = new APIService();
